ef5b6e8d19e8fc142cc0cba7a2d322a074018a16
AndreaLotti <andrealotti88@gmail.com>
2020-11-15 19:24:13 +0100

    bug fix

--- src/main/java/GUIclient/Client.java
+++ src/main/java/GUIclient/Client.java
@@ -3,65 +3,105 @@ package GUIclient;
 import java.io.*;
 import java.net.*;
 
-public class Client {
+public class Client{
+
     private final static int serverPort = 6789;
     private Socket s;
     private BufferedReader input;
     private BufferedReader inFromServer;
     private DataOutputStream outToServer;
-    private SendThread sendThread;
-    private ReadThread readThread;
-    private String username;
-
-    public void connect(String username) {
+    private ReadThread read;
+    private SendThread send;
+    String username;
+    
+    public Client(String username) {
         this.username = username;
+        connetti();
+    }
+    
+    public void connetti(){
         //Inizializzazione input
         input = new BufferedReader(new InputStreamReader(System.in));
-
+        
         try {
             //Recupero ip localhost
-            InetAddress ip = InetAddress.getByName("localhost");
-
+            InetAddress ip = InetAddress.getByName("localhost"); 
+          
             //Conessione al server
-            s = new Socket(ip, serverPort);
+            s = new Socket(ip, serverPort); 
 
             // Apertura canali
             inFromServer = new BufferedReader (new InputStreamReader (s.getInputStream()));
-            outToServer = new DataOutputStream(s.getOutputStream());
-     
-            sendThread = new SendThread();
-            readThread = new ReadThread();
+            outToServer = new DataOutputStream(s.getOutputStream()); 
+            
+            send = new SendThread();
+            read = new ReadThread();
         }
         catch(Exception ex) {
-            ex.toString();
-        }
+           ex.toString();
+        }    
     }
     
-    public String getUsername() {
-        return username;
-    }
-    
-    public void sendMex(String mex) {
-        sendThread.send(mex);
-    }
-
-    private class SendThread extends Thread{
+    public class SendThread extends Thread{
         public SendThread() {
             start();
         }
-        
-        public void send(String mex) {
-            try{
-               outToServer.writeBytes(mex); 
-            } catch (Exception ex) {
-                System.out.println("Impossibile inviare il messaggio");
-            }         
+
+        @Override
+        public void run() {
+            try {
+                outToServer.writeBytes(username + '\n');                      
+            }
+            catch (Exception ex) {
+                ex.toString();
+                System.exit(1);
+            }
+        }
+
+        public void close() {
+            try {
+                inFromServer.close();
+                outToServer.close();
+                s.close();
+                read.close();
+                this.stop();
+            }
+            catch(Exception ex) {
+                ex.toString();
+            }
+            System.exit(0);
         }
     }
 
-    private class ReadThread extends Thread{
+    public class ReadThread extends Thread{
+        String msg;
         public ReadThread() {
             start();
         }
+
+        @Override
+        public void run() {
+            try {
+                for(;;) {
+                   msg = inFromServer.readLine();
+                }
+            }
+            catch (Exception ex) {
+                ex.toString();
+                System.exit(1);
+            }
+        }
+
+        public void close() {
+            try {
+                this.stop();
+            }
+            catch(Exception ex) {
+                ex.toString();
+            }
+            System.exit(0);
+        }
     }
+    
 }
+    
--- src/main/java/GUIserver/GestioneChat.java
+++ src/main/java/GUIserver/GestioneChat.java
@@ -1,65 +1,31 @@
 package GUIserver;
-import GUIclient.*;
 
+import GUIclient.FrameChat;
+import GUIclient.FrameClient;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
 public class GestioneChat{
-    private ArrayList<ServerThread> sockets = new ArrayList();
-    private ArrayList<String> utenti = new ArrayList();
-    private ArrayList<FrameChat> frameChats = new ArrayList();
+    private ArrayList<ServerThread> sockets = new ArrayList<>();
+    private ArrayList <FrameChat> chats = new ArrayList<>();
     private ServerSocket s;
-    private static GestioneChat instance = null;
-
-    //Utilizziamo un singleton della gestione chat perche i client devono fare riferimento allo stesso server
-    public static GestioneChat getInstance() {
-        if(instance == null) instance = new GestioneChat();
-        return instance;
-    }
     
-    //avvia il server che si mette in ascolto sul socket
     public void start(){
-        getInstance();
         try{
             System.out.println("server in attesa");
-            instance.s = new ServerSocket(6789);
-            
+            s = new ServerSocket(6789);
+
             for(int i = 0; i < 10; i++) {
-                instance.sockets.add(new ServerThread(instance.s.accept()));
-                instance.sockets.get(i).start();
+                sockets.add(new ServerThread(s.accept()));
+                sockets.get(i).start();
             }
-            instance.s.close();
+            s.close();
         }catch (Exception e){
             System.out.println(e.getMessage());
             System.out.println("Errore durante l'istanza del messaggio!");
             System.exit(1);
         }
-    }
-    
-    //controlla se l'username è già presente, altrimenti lo inserisce
-    public boolean controlloUsername(String username){      
-        for(String u : instance.utenti){
-            if(username.equalsIgnoreCase(u)) {
-               return false;
-            }
-        }               
-        instance.utenti.add(username);       
-        return true;
-    }
-
-    public boolean addFrameChat(FrameChat fc) {
-        if(instance.frameChats.add(fc)) {
-            for(FrameChat fch : instance.frameChats) {
-                fch.updateUserList();
-            }
-            return true;
-        }   
-        return false;
-    }    
-
-    public ArrayList<String> getUtenti() {
-        return instance.utenti;
     }
     
     public static void main(String[] args) {
@@ -68,26 +34,54 @@ public class GestioneChat{
     }
 
     public class ServerThread extends Thread{
-        private Socket clientS;
-        private BufferedReader inDalClient;
-        private DataOutputStream outVersoClient;
-        private String usernameClient;
-
+            private Socket clientS;
+            private BufferedReader inDalClient;
+            private DataOutputStream outVersoClient;
+            public String usernameClient;
+            FrameChat frm = new FrameChat(usernameClient);  
+    
         public ServerThread (Socket socket){
-            this.clientS = socket;
+                this.clientS = socket;
         }
 
         @Override
         public void run (){
             try{
-                comunica();
-            } catch (Exception e){
+               chats.add(frm);
+               frm.setVisible(true);
+               for(FrameChat f : chats)
+               {
+                    f.updateUserList();
+               }
+            }catch (Exception e){
                 e.toString();
             }
         }
-
-        public void comunica () throws Exception {
-            
+        
+        public void sendMex(String s){ 
+            try{
+                String[] mexSplit = s.split("@");
+                if(sockets.size() > 1) {
+                    if(mexSplit[1].equalsIgnoreCase("Everyone")) {         
+                        for (ServerThread n : sockets) {
+                            if(n != this) n.outVersoClient.writeBytes(usernameClient + ": " + mexSplit[0] + '\n');
+                        }
+                    }
+                }
+                else {
+                    for (ServerThread n : sockets) {
+                        if(n != this){
+                            if(n.usernameClient.equals(mexSplit[1])) {
+                                n.outVersoClient.writeBytes(usernameClient + ": " + mexSplit[0] + '\n');
+                                break;
+                            }
+                        }
+                    }
+                }   
+            }catch(IOException e){
+                    e.toString();      
+            }
         }
     }
 }
+    
